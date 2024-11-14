66f44ff15e560cc0578fb3c90defdd80c988caf0
ingui-n <inguin@email.cz>
2024-11-14 13:18:26 +0100

    removed cloudflare

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import {getAllLatestTemperature} from "@/app/actions";
 import TemperatureDashboard from "@/components/TemperatureDashboard";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function Root() {
   const latestTemperatures = await getAllLatestTemperature();
 
