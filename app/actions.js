681918e874f0cb085cd81739a34e93dfa5d4f766
ingui-n <inguin@email.cz>
2024-11-23 18:36:56 +0100

    updated temperature load to view

--- app/actions.js
+++ app/actions.js
@@ -1,36 +1,7 @@
 import {pocketBase} from "@/lib/pocketbase";
 
-export const getLatestTemperature = async (device) => {
+export const getLatestTemperature = async () => {
   const pb = await pocketBase();
 
-  const temperature = await pb.collection('temperature').getList(1, 1, {
-    sort: '-created',
-    filter: `device = "${device}"`
-  });
-
-  return temperature.items.length === 1 ? temperature.items[0] : null;
-};
-
-export const getAllDevices = async () => {
-  const pb = await pocketBase();
-
-  return await pb.collection('devices').getList(1, 100);
-};
-
-export const getAllLatestTemperature = async () => {
-  const devices = await getAllDevices();
-
-  const temperatures = [];
-
-  for (const device of devices.items) {
-    const temperature = await getLatestTemperature(device.device);
-
-    if (!temperature)
-      continue;
-
-    temperature.name = device.name;
-    temperatures.push(temperature);
-  }
-
-  return temperatures;
+  return await pb.collection('latest_temperature').getList(1, 100);
 };
--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import {getAllLatestTemperature} from "@/app/actions";
+import {getLatestTemperature} from "@/app/actions";
 import TemperatureDashboard from "@/components/TemperatureDashboard";
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default async function Root() {
-  const latestTemperatures = await getAllLatestTemperature();
+  const latestTemperatures = await getLatestTemperature();
 
   return (
     <TemperatureDashboard
